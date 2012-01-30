b0ba8d971e7b72329906ff58380acf931160bb55
ruanyf <ruanyf@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-30 06:22:03 +0000

    tagging version 0.2.1
    git-svn-id: https://plugins.svn.wordpress.org/untco/tags/0.2.1@497199 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- untco.php
+++ untco.php
@@ -5,7 +5,7 @@ Plugin URI: http://www.ruanyifeng.com/webapp/untco.html
 Description: This plugin is based on Twitter Tools. When a post is created automatically from a tweet, this plugin will unshorten all t.co short URLs in the tweet into their original URLs.
 Version: 0.2
 Author: Ruan YiFeng
-Author URI: http://www.ruanyieng.com
+Author URI: http://www.ruanyifeng.com
 License: GPL2
 */
 
